d73ff820460c5ac7518452e4b2810db83a78cb7c
Nils <nidresch@googlemail.com>
2018-06-22 04:52:51 +0200

    page routing

--- pages/index/src/index.tsx
+++ pages/index/src/index.tsx
@@ -1,22 +1,8 @@
 import * as React from "react";
 import { render } from 'react-dom';
-
-class IndexComponent extends React.Component<any, any>
-{
-    constructor(props: any) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                HELLO WORLD FROM REACT!
-            </div>
-        );
-    }
-}
+import PageRouter from './router';
 
 render(
-    <IndexComponent />,
+    <PageRouter />,
     document.getElementById('container')
 );
---
+++ pages/index/src/views/home/homePage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+export default class HomePage extends React.Component<any, any> {
+
+    public render() {
+        return (
+            <div>
+                AY
+            </div>
+        );
+    }
+
+}
