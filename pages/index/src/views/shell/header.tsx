c0130263e7795ac1dd13060e3e569cbd99869c6b
Nils <nidresch@googlemail.com>
2018-06-23 09:38:15 +0200

    move searchbar to components

--- pages/index/src/views/shell/header.tsx
+++ pages/index/src/views/shell/header.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import HeaderSearchbar from './header-searchbar';
+import HeaderSearchbar from '../../components/searchbar/header-searchbar';
 
 export default class Header extends React.Component<any, any> {
 	public render() {
