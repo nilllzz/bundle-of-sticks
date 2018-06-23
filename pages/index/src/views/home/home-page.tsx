2ad8579862520af202bbf0cdf72671694dfc2b8a
Nils <nidresch@googlemail.com>
2018-06-23 09:49:38 +0200

    make nav global

--- pages/index/src/views/home/home-page.tsx
+++ pages/index/src/views/home/home-page.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import Nav from '../../components/nav/nav';
 
 export default class HomePage extends React.Component<any, any> {
 	public render() {
-		return (
-			<div className="home-main">
-				<div className="home-left shell-split-content-left">
-					<Nav location={this.props.location.pathname} />
-				</div>
-				<div className="home-right shell-split-content-right">some gay ass content</div>
-			</div>
-		);
+		return <div className="home-main">some gay ass content</div>;
 	}
 }
--- pages/index/src/views/shell/main.tsx
+++ pages/index/src/views/shell/main.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 import Footer from './footer';
 import Header from './header';
+import Nav from '../../components/nav/nav';
 
 export default class Shell extends React.Component<any, any> {
 	public render() {
+		const path = window.location.pathname;
 		return (
 			<div className="shell-main-body">
 				<Header />
 				<div className="shell-main-content">
-					<div className="shell-main-children">{this.props.children}</div>
+					<div className="shell-split-content">
+						<div className="shell-split-content-left">
+							<Nav location={path} />
+						</div>
+						<div className="shell-split-content-right">
+							<div className="shell-main-children">{this.props.children}</div>
+						</div>
+					</div>
 				</div>
 				<Footer />
 			</div>
